d05e63a3d2e63cde23842f63b91b0e97a8f37c50
Luzaks <kanemekanik@gmail.com>
2020-06-20 12:47:28 -0500

    Feat: Organize files in it's containing folders.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import '../styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Smart calculator
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="display-container">
+        <h1>Display</h1>
+      </div>
+    );
+  }
+}
+
+export default Display;
