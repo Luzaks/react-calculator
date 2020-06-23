b066f6a7e6cdfae059bc0dd93dca6dc35398b68f
Luzaks <kanemekanik@gmail.com>
2020-06-23 13:49:29 -0500

    Test: Add dependency for testing, add test for App component.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from '../components/App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Smart calculator/i);
-  expect(linkElement).toBeInTheDocument();
+  const { queryAllByText } = render(<App />);
+  const zeroElement = queryAllByText(/0/i);
+  expect(typeof zeroElement).toBe('object');
 });
